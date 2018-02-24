2bf3058b43ddf31e3e34e13eae1dbf749e00b7be
poxstone <oscar.ortiz@xertica.com>
2018-02-24 14:17:13 -0500

    change name servlet

--- src/test/java/com/poxstone/SampleServlet.java
+++ src/test/java/com/poxstone/SampleServlet.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class HelloAppEngineTest {
+public class SampleServlet {
 
   @Test
   public void test() throws IOException {
